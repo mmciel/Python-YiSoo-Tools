34f779fc9456d5334812680ac80b18bd65267581
mmciel <761998179@qq.com>
2019-01-30 20:57:17 +0800

    in

--- auto.py
+++ auto.py
@@ -1,11 +1,3 @@
-from urllib import request
-
-
-url = "http://gslb.miaopai.com/stream/Z7vERUMvdMg36uCHJ1vRZxWhXSzLWnQ-paynhw__.mp4?yx=&refer=weibo_app&vend=weibo&label=mp4_hd&mpflag=8&Expires=1548767462&ssig=dXdKkh5eV3&KID=unistore,video"
-# 文件的方式保存下载的图片
-response = request.urlopen(url)
-#读取返回的内容
-img = response.read()
-with open('temp.mp4','wb') as f:
-    #将图片内容以二进制写入
-    f.write(img)
+index_file = open('YiSooValue/index.html', 'r', encoding="utf-8")
+index_context = index_file.read()
+print(index_context)
